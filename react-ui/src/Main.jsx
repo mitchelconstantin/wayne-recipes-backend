3f7b2ea2a828ea9db5d9806108359b6f9d861490
Mitchel Constantin <mitchel.constantin@rigup.com>
2020-01-27 18:34:33 -0600

    delete redundant router

--- react-ui/src/Main.jsx
+++ react-ui/src/Main.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
-import Recipe from './Recipe'
 import UpdateRecipe from './UpdateRecipe/UpdateRecipe'
-import {ShoppingList} from './ShoppingList/ShoppingList'
+import { ShoppingList } from './ShoppingList/ShoppingList'
 import { Switch, Route, Redirect } from 'react-router-dom'
+import Home from './AllRecipes/Home'
+import Login from './Login/Login'
+import RecipeDisplay from './ShowRecipe/RecipeDisplay';
 
 function Main() {
   return (
     <main>
-        <Switch>
-        <Route path='/login' component={Recipe} />
-          <Route path='/all' component={Recipe} />
-          <Route path='/list' component={ShoppingList} />
-          <Route path='/r' component={Recipe} />
-          <Route path='/new' component={UpdateRecipe} />
-          {/* <Route path='/r/:number/edit' component={UpdateRecipe} /> */}
-
-          <Redirect from="/" to="/all" />
+      <Switch>
+        <Route path='/login' component={Login} />
+        <Route exact path='/all' component={Home} />
+        <Route path='/list' component={ShoppingList} />
+        <Route path='/new' component={UpdateRecipe} />
+        <Route exact path='/r/:number' component={RecipeDisplay} />
+        <Route path='/r/:number/edit' component={UpdateRecipe} />
+        <Redirect from="/" to="/all" />
       </Switch>
     </main>
   );
