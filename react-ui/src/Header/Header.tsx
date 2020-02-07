674b70e337f8a89757a15f9493e099ce91849b84
Mitchel Constantin <mitchconstantin@gmail.com>
2020-02-07 09:34:14 -0600

    combine extra buttons into popout menu

--- react-ui/src/Header/Header.tsx
+++ react-ui/src/Header/Header.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Box, AppBar, Toolbar, Typography, Button } from '@material-ui/core/';
-import logo from './logo.svg'
-import { ShoppingListBehaviors } from '../ShoppingList/ShoppinglistBehaviors'
+import {
+  Box,
+  Menu,
+  AppBar,
+  Toolbar,
+  Typography,
+  Button
+} from '@material-ui/core/';
+import logo from './logo.svg';
+import { ShoppingListBehaviors } from '../ShoppingList/ShoppinglistBehaviors';
 import { isLoggedIn, logOut, isAdmin, isOwner } from '../Shared/AppBehaviors';
 const useStyles = makeStyles(theme => ({
   root: {
     // flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(4),
   },
   title: {
     marginRight: 'auto'
@@ -21,59 +25,117 @@ const useStyles = makeStyles(theme => ({
   },
   button: {
     color: 'white'
-  }
+  },
+  menuButton: {}
 }));
 
 export const Header = () => {
   const [Container, TopRow, ButtonSet, Center] = [Box, Box, Box, Box, Box];
   const classes = useStyles();
-  const title = `WAYNE'S FAMILY RECIPES`
-  const description = 'Traditional Cajun food and so much more!'
+  const title = `WAYNE'S FAMILY RECIPES`;
+  const description = 'Traditional Cajun food and so much more!';
   const ShoppingListLabel = () => {
     const text = 'Shopping List';
     const list = ShoppingListBehaviors.load();
     if (!list.length) return text;
-    return `${text}(${list.length})`
-  }
+    return `${text}(${list.length})`;
+  };
   const UserButtons = () => {
+    const [anchorEl, setAnchorEl] = useState(null);
+    //@ts-ignore
+    const handleClick = event => {
+      setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
 
     return (
-      <ButtonSet ml='auto'>
-        {!!isLoggedIn() ?
-          <Button onClick={logOut} className={classes.button}>Logout</Button>
-          :
-          <Button href='/login' className={classes.button}>Login</Button>
-        }
-        {isAdmin() && <Button href='/new' className={classes.button} >Add new recipe</Button>}
-        {isOwner() && <Button href='/dashboard' className={classes.button} >Admin Dashboard</Button>}
-        <Button href='/list' className={classes.button}>{ShoppingListLabel()}</Button>
-        <Button href='/all' className={classes.button}>All Recipes</Button>
+      <ButtonSet ml="auto">
+        <Button href="/all" className={classes.button}>
+          All Recipes
+        </Button>
+        <Button onClick={handleClick} className={classes.button}>
+          Menu
+        </Button>
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <Box display="flex" flexDirection="column">
+            {!!isLoggedIn() ? (
+              <Button onClick={logOut} className={classes.menuButton}>
+                Logout
+              </Button>
+            ) : (
+              <Button href="/login" className={classes.menuButton}>
+                Login
+              </Button>
+            )}
+            {isAdmin() && (
+              <Button href="/new" className={classes.menuButton}>
+                Add new recipe
+              </Button>
+            )}
+            {isOwner() && (
+              <Button href="/dashboard" className={classes.menuButton}>
+                Admin Dashboard
+              </Button>
+            )}
+            <Button href="/list" className={classes.menuButton}>
+              {ShoppingListLabel()}
+            </Button>
+          </Box>
+        </Menu>
       </ButtonSet>
-    )
-  }
+    );
+  };
 
   return (
     <div className={classes.root}>
-      <AppBar position="fixed" >
+      <AppBar position="fixed">
         <Toolbar className={classes.toolbar} disableGutters>
-          <Center display='flex' flexDirection='row' alignItems='center' width='100%' >
-            <img src={logo} alt={'Logo'} style={{ height: '40px', width: '40px', marginRight: '30px' }} />
-            < UserButtons />
+          <Center
+            display="flex"
+            flexDirection="row"
+            alignItems="center"
+            width="100%"
+          >
+            <img
+              src={logo}
+              alt={'Logo'}
+              style={{ height: '40px', width: '40px', marginRight: '30px' }}
+            />
+            <UserButtons />
           </Center>
         </Toolbar>
       </AppBar>
       <AppBar position="sticky">
         <Toolbar className={classes.toolbar} disableGutters>
-          <Container display='flex' flexDirection='column' width='100%'>
-            <TopRow display='flex' justifyContent='top' mb='30px' mt='20px' width='100%' >
-              <img src={logo} alt={'Logo'} style={{ height: '80px', width: '80px', }} />
-              < UserButtons />
+          <Container display="flex" flexDirection="column" width="100%">
+            <TopRow
+              display="flex"
+              justifyContent="top"
+              mb="30px"
+              mt="20px"
+              width="100%"
+            >
+              <img
+                src={logo}
+                alt={'Logo'}
+                style={{ height: '80px', width: '80px' }}
+              />
+              <UserButtons />
             </TopRow>
-            <Center display='flex' flexDirection='column' mb='20px'>
-              <Typography className={classes.title} variant="h2" >
+            <Center display="flex" flexDirection="column" mb="20px">
+              <Typography className={classes.title} variant="h2">
                 {title}
               </Typography>
-              <Typography className={classes.title} variant="h6" >
+              <Typography className={classes.title} variant="h6">
                 {description}
               </Typography>
             </Center>
@@ -81,5 +143,5 @@ export const Header = () => {
         </Toolbar>
       </AppBar>
     </div>
-  )
-}
+  );
+};
