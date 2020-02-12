7344cd323eb9b4e3413f4a592b919161cf3ba2dc
Mitchel Constantin <mitchconstantin@gmail.com>
2020-02-12 09:26:23 -0600

    use new library for lazyloading to fix scrolling issue

--- react-ui/src/AllRecipes/RecipeCard.tsx
+++ react-ui/src/AllRecipes/RecipeCard.tsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import noImage from '../Shared/noImage.png'
+import React from 'react';
+import noImage from '../Shared/noImage.png';
 import { makeStyles } from '@material-ui/core/styles';
-import { Box, Paper, Typography, } from '@material-ui/core/';
+import { Box, Paper, Typography } from '@material-ui/core/';
 import { IRecipe } from '../Shared/Types';
-import LazyLoad from 'react-lazyload';
+//@ts-ignore
+import { LazyLoadComponent} from 'react-lazy-load-image-component';
 
-const useStyles = makeStyles(theme => ({ 
+const useStyles = makeStyles(theme => ({
   card: {
     cursor: 'pointer',
     margin: '20px',
-    width: '300px',
-  },
+    width: '300px'
+  }
 }));
 
 interface Props {
@@ -19,22 +20,34 @@ interface Props {
 
 export const RecipeCard = ({ recipe }: Props) => {
   const classes = useStyles();
-  const selectRecipe = () => window.location.href = `/r/${recipe.id}`;
+  const selectRecipe = () => (window.location.href = `/r/${recipe.id}`);
   const onError = (ev: any) => {
     const eventTarget = ev.target;
     eventTarget.src = noImage;
   };
+
+  interface ImgProps {
+  picture?: string;
+}
+  const RecipeImg = ({picture}: ImgProps) => (
+    <img
+      onError={onError}
+      src={picture || noImage}
+      alt={'a tasty dish!'}
+      style={{ height: '300px', width: '300px' }}
+    />
+  );
+
   return (
     <Box justifySelf="center" alignSelf="center">
       <Paper className={classes.card} onClick={selectRecipe}>
-        <LazyLoad height={200}>
-          <img
-            onError={onError}
-            src={recipe.picture || noImage}
-            alt={'a tasty dish'}
-            style={{ height: '300px', width: '300px' }}
-          />
-        </LazyLoad>
+        <LazyLoadComponent
+          visibleByDefault={true} 
+          threshold={100}
+          placeholder={<RecipeImg />}
+        >
+          <RecipeImg picture={recipe.picture} />
+        </LazyLoadComponent>
         <Box p="10px">
           <Typography>{recipe.title}</Typography>
           <Typography>{`from: ${recipe.source || 'unknown'}`}</Typography>
