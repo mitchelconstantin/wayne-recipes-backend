ddba1153bbd5952102df1977e49d7d21f999fde6
Mitchel Constantin <mitchconstantin@gmail.com>
2020-03-03 11:05:16 -0600

    show loading bar on home page

--- react-ui/src/Shared/Components/Loading.tsx
+++ react-ui/src/Shared/Components/Loading.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import { LinearProgress } from '@material-ui/core';
+import { Box, LinearProgress } from '@material-ui/core';
 
 export const Loading = () => {
-  return <LinearProgress  />;
+  return (
+    <Box width={'100%'}>
+      <LinearProgress />
+    </Box>
+  );
 };
