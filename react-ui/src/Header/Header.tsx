fd35e4467172fee6f2a633a06da6a24bbae8a107
Mitchel Constantin <mitchconstantin@gmail.com>
2020-03-12 08:31:05 -0500

    use breakpoints in header

--- react-ui/src/Header/Header.tsx
+++ react-ui/src/Header/Header.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import { Box } from '@material-ui/core/';
+import { Box, makeStyles } from '@material-ui/core/';
 import { SmallAppBar } from './SmallAppBar';
 import { LargeAppBar } from './LargeAppBar';
-import { isMobile } from '../Shared/AppBehaviors';
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    [theme.breakpoints.down('sm')]: {
+      paddingBottom: '70px'
+    },
+    [theme.breakpoints.up('md')]: { paddingBottom: '0px' }
+  }
+}));
 
 export const Header = () => {
+  const classes = useStyles();
   return (
-    <Box paddingBottom={isMobile() ? '70px' : '0px'} displayPrint="none">
+    <Box className={classes.container} displayPrint="none">
       <SmallAppBar />
       <LargeAppBar />
     </Box>
