59281d7a7dae05b6f62b79f9c789093f65e3aa90
Mitchel Constantin <mitchconstantin@gmail.com>
2020-05-01 10:37:21 -0500

    init backend split for recipe app

--- react-ui/src/react-app-env.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react-scripts" />
-
-declare module 'react-lazy-load-image-component';
-declare module 'react-router-dom';
-declare module 'fuzzaldrin-plus';
-declare module 'lodash';
