337137902e1115c4cfd9a7fe7c906af15a937c5e
Mitchel Constantin <mitchconstantin@gmail.com>
2020-05-02 07:50:58 -0500

    add cors header

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express()
 
 const bodyParser = require('body-parser');
 
+app.use(cors());
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
