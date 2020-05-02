3babfb9b36b921a1cc6c565c6b11aaa7bf2505f4
Mitchel Constantin <mitchconstantin@gmail.com>
2020-05-02 07:53:14 -0500

    import cors

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const morgan = require('morgan')
-
+const cors = require('cors');
 const api = require('./api')
 
 const app = express()
