853e6464ffd500e3b1e81cfc69e0f66f9c3a331f
Mitchel Constantin <mitchelc@ifit.com>
2021-03-16 16:17:12 -0500

    try adding SSL=true

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,11 @@
+const addSSL = (url: string) => {
+  if (url.endsWith("?ssl=true")) return url;
+  return `${url}?ssl=true`;
+};
+
 export const config = {
   secret: process.env.USER_AUTH_SECRET,
   isLocal: process.env.IS_LOCAL,
-  testDb: process.env.TEST_DB,
-  prodDb: process.env.HEROKU_POSTGRESQL_WHITE_URL,
+  testDb: addSSL(process.env.TEST_DB),
+  prodDb: addSSL(process.env.HEROKU_POSTGRESQL_WHITE_URL),
 };
