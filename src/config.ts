d5aa1f4399f5c125379d2d615e6ca0cceaa9fedf
Mitchel Constantin <mitchconstantin@gmail.com>
2021-09-15 09:20:14 -0400

    bump deps

--- src/config.ts
+++ src/config.ts
@@ -7,8 +7,6 @@ const addSSL = (url: string) => {
 export const config = {
   secret: process.env.USER_AUTH_SECRET,
   isLocal: process.env.IS_LOCAL,
-  // testDb: addSSL(process.env.TEST_DB),
-  // prodDb: addSSL(process.env.HEROKU_POSTGRESQL_WHITE_URL),
   testDb: process.env.TEST_DB,
   prodDb: process.env.HEROKU_POSTGRESQL_WHITE_URL,
 };
